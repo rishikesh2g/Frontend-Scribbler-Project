79d0788c07d4fc64e77fdfc2e3cb97e2d4ce011a
rishikesh2g <60296904+rishikesh2g@users.noreply.github.com>
2021-06-28 11:04:40 +0530

    initial

---
+++ html/All post.js
@@ -0,0 +1,64 @@
+function togglePopup() {
+    document.getElementById("popup-1")
+     .classList.toggle("active");
+   }
+function togglePopuphtml() {
+    document.getElementById("popup-2")
+    .classList.toggle("active");}
+
+    function togglePopupcomment() {
+      document.getElementById("popup-3")
+       .classList.toggle("active");
+     }
+
+
+
+    /* function myFunction() {
+      var dots = document.getElementById("dots");
+      var moreText = document.getElementById("more");
+      var btnText = document.getElementById("myBtn");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }*/
+
+
+    
+    var modal = document.getElementById('id01');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var modal = document.getElementById('id02');
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+var modal = document.getElementById('id03');
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+var modal = document.getElementById('id04');
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
